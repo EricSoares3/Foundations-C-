7a5af4c11edebfa32fdc022826dc4b322ec6579c
Eric Soares <contato.eric.sm@gmail.com>
2018-11-25 23:18:10 -0200

    Restrições e convenções para nomes

--- FoundationsCsharp/Program.cs
+++ FoundationsCsharp/Program.cs
@@ -1,35 +1,18 @@
 ﻿using System;
 
-namespace FoundationsCsharp
+namespace Curso // Pascal case 
 {
-    class Program
+    class ContaBancaria // Pascal case
     {
-        static void Main(string[] args)
+        public string Titular /*Propriedade = pascal case*/ { get; set; }
+        private double _saldo /* atributo interno da classe = padrão */;
+        public void Deposito /*Nome de método = pascal case */  (double quantia /*parâmetro = camel case */)
         {
-            bool completo = false;
-            char genero = 'F';
-            char letra = '\u0041';
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
-            float n5 = 4.5f;
-            double n6 = 4.5;
-            String nome = "Maria Green";
-            Object obj1 = "Alex Brown";
-            Object obj2 = 4.5f;
-            Console.WriteLine(completo);
-            Console.WriteLine(genero);
-            Console.WriteLine(letra);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
-            Console.WriteLine(nome);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
+            _saldo /*atributo interno de um método = padrão */ += quantia /*atributo interno de um método = padrão */ ;
+        }
+        public double GetSaldo /*Nome de método = pascal case */ ()
+        {
+            return _saldo /*atributo interno de um método = padrão */ ;
         }
     }
 }
