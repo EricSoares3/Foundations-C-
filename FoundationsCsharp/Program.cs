90d5dba3ceac3710737b428f2d0c5d2b9be93b90
Eric Soares <contato.eric.sm@gmail.com>
2018-11-25 23:22:41 -0200

    Saída de dados em C#

--- FoundationsCsharp/Program.cs
+++ FoundationsCsharp/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Curso // Pascal case 
+namespace PrimeiroProjeto
 {
-    class ContaBancaria // Pascal case
+    class Program
     {
-        public string Titular /*Propriedade = pascal case*/ { get; set; }
-        private double _saldo /* atributo interno da classe = padrão */;
-        public void Deposito /*Nome de método = pascal case */  (double quantia /*parâmetro = camel case */)
+        static void Main(string[] args)
         {
-            _saldo /*atributo interno de um método = padrão */ += quantia /*atributo interno de um método = padrão */ ;
-        }
-        public double GetSaldo /*Nome de método = pascal case */ ()
-        {
-            return _saldo /*atributo interno de um método = padrão */ ;
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            String nome = "Maria";
+
+            // A diferença entre Console.write e Console.writeLine é que o sem Line ele não faz uma quebra de linha.
+
+
+            Console.Write("Bom dia!"); 
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2")); // O método toString("F2") é usado para delimitar quantos números irão vir após a virgula
+            Console.WriteLine(saldo.ToString("F4")); // O método toString("F2") é usado para delimitar quantos números irão vir após a virgula
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); // O outro parâmetro passado para o método toString, no caso
+            // CultureInfo.InvariantCulture, faz com que ele ignore todos os tipos de delimitação de cada país ( seja virgula, etc ) para apenas ponto ( . ) 
         }
     }
 }
