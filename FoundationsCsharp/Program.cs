bbddcce0ded12c4602f5b924a4628d29e21100c1
soareseric <contato.eric.sm@gmail.com>
2018-11-26 22:12:08 -0200

    Conversão implicita e casting

--- FoundationsCsharp/Program.cs
+++ FoundationsCsharp/Program.cs
@@ -8,29 +8,30 @@ namespace PrimeiroProjeto
         static void Main(string[] args)
         {
 
-            int n1 = 3 + 4 * 2; // sem parênteses o grau de importância será divisão e multiplicação primeiro
+            // Casting, e conversão implicita
 
-            int n2 = (3 + 4) * 2; // parênteses tem grau de importância maior, assim començando por quem está dentro dela
+            float e = 2;
+            double t = e;
+            Console.WriteLine(t); // conversão implicita 
 
-            int n3 = 17 % 3; // operador mod, resposta é o resto da divisão
 
-            double n4 = 10.0 / 8.0; // se fosse int, iria retornar apenas 1, devido a não ser um ponto flutuante
 
-            double a = 1.0, b = -3.0, c = -4.0;
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a; // casting double para float
+            Console.WriteLine(b);
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c; // Math pow é uma função matematica ja pronto, para calcular potência
+            double x;
+            int z;
+            x = 5.1;
+            z = (int)x; // casting double para int
+            Console.WriteLine(z);
 
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a); // Math sqrt é uma função matematica para calcular raiz quadrada
-
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a); // Math sqrt é uma função matematica para calcular raiz quadrada
-
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            int c = 5;
+            int d = 2;
+            double resultado = (double)c / d; // casting do resultado da divisão de dois int para double
+            Console.WriteLine(resultado);
         }
     }
 }
