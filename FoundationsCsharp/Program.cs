6a98e338697376783b890d1d42b1c591bb9a745f
soareseric <contato.eric.sm@gmail.com>
2018-11-26 23:01:33 -0200

    Estrutura condicional If / else

--- FoundationsCsharp/Program.cs
+++ FoundationsCsharp/Program.cs
@@ -7,15 +7,16 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            bool c1 = 2 > 3 || 4 != 5; // true
-            bool c2 = !(2 > 3) && 4 != 5; // true
-            Console.WriteLine(c1);
-            Console.WriteLine(c2);
-            Console.WriteLine("--------------");
-            bool c3 = 10 < 5; // false
-            bool c4 = c1 || c2 && c3; // true
-            Console.WriteLine(c3);
-            Console.WriteLine(c4);
+            Console.WriteLine("Entre com um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
         }
     }
 }
